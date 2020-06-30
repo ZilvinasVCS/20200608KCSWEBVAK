e57ee2282c83b3b0daf7bafebbc9ca8fc74a5884
zilvinasvcs <zilvinasvcs@gmail.com>
2020-06-30 21:20:32 +0300

    14os paskaitos medziaga. abi dalys.

--- classwork/14PHP2/index.php
+++ classwork/14PHP2/index.php
@@ -4,22 +4,32 @@
 	echo $testText;
 
 	$studentsNames = [
-		'male' => ['Andrius', 'Tomas', 'Benas'],
-		'female' => ['Alina']
+		'male_names' => ['Andrius', 'Tomas', 'Benas'],
+		'female_names' => ['Alina', 'Eglė']
 	];
 
-	$studentsNames['male'][1];
-
-	$studentsNames['female'][] = 'Nijolė';
+	$studentsNames['male_names'][1];
+	$studentsNames['female_names'][] = 'Nijolė';
 
 	//echo $studentsNames[3]; // echo only one array element
 	$studentsNames['Male'] = 'Giedrius';
 	print_r($studentsNames);
 	echo count($studentsNames); //suskaiciuoja masyvo nariu vienetus.
 	echo '<br><br>';
-	asort($studentsNames['male']);
-	echo implode(', ', $studentsNames['male']);
+	asort($studentsNames['male_names']);
+	echo implode(', ', $studentsNames['male_names']);
 	echo '<br><br>';
 
+	/* for, while */
+
+	for ($i = 0; $i < count($studentsNames['male_names']); $i++) { ?>
+		<script>
+			console.log('<?php echo $studentsNames['male_names'][$i]; ?>!');
+		</script>
+	<?php }
+
+	foreach ($studentsNames['female_names'] as $studentName) {
+		echo $studentName;
+	}
 
 ?>
