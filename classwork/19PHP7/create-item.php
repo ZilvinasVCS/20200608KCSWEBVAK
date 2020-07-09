9472a600adb6d130672e9fa27e49cdd43d431e67
zilvinasvcs <zilvinasvcs@gmail.com>
2020-07-09 20:14:17 +0300

    devynioliktos paskaitos pirma dalis

---
+++ classwork/19PHP7/create-item.php
@@ -0,0 +1,11 @@
+<?php
+	$pageTitle = 'Create new item';
+	include('layout-header.php');
+
+?>
+
+Prideti/add new item to the list.
+
+<?php
+	include('layout-footer.php');
+?>
---
+++ classwork/19PHP7/index.php
@@ -0,0 +1,11 @@
+<?php
+	$pageTitle = 'All items';
+	include('layout-header.php');
+
+?>
+
+Musu daiktai musu namuose
+
+<?php
+	include('layout-footer.php');
+?>
